10050ed2ba50372cf57e10821cf9c4283a762f74
Parker <parkerjlandon@gmail.com>
2021-01-06 18:10:56 -0800

    Hello, GitHub.

---
+++ cmake-build-debug/CMakeFiles/CLion_Hello_World.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Parker/Desktop/SPU CS/2430 Data Structures I/Landon_P CLion Hello World GitHub/CLion-Hello-World/main.cpp" "C:/Users/Parker/Desktop/SPU CS/2430 Data Structures I/Landon_P CLion Hello World GitHub/CLion-Hello-World/cmake-build-debug/CMakeFiles/CLion_Hello_World.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CLion_Hello_World.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLion_Hello_World.exe"
+  "CLion_Hello_World.exe.manifest"
+  "CLion_Hello_World.lib"
+  "CLion_Hello_World.pdb"
+  "CMakeFiles/CLion_Hello_World.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_Hello_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
